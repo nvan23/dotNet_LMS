03ff68db726f06d60d6e4eb76f5c27bebff39c63
nvan23 <dtnvan230898@gmail.com>
2020-05-05 18:41:27 +0700

    Create adding book function

---
+++ LibraryManagementSystem/LibraryManagementSystem/add_books.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace LibraryManagementSystem
+{
+    partial class add_books
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.txtBookAuthorName = new System.Windows.Forms.TextBox();
+            this.txtBookPublication = new System.Windows.Forms.TextBox();
+            this.txtBookPrice = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtBookID = new System.Windows.Forms.TextBox();
+            this.dtpBookPurchaseDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(301, 300);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(207, 300);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 15;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Book Author Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Book Publication";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Book Purchase Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Book Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(86, 262);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Book Quantity";
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Location = new System.Drawing.Point(207, 88);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(200, 20);
+            this.txtBookName.TabIndex = 6;
+            // 
+            // txtBookAuthorName
+            // 
+            this.txtBookAuthorName.Location = new System.Drawing.Point(207, 122);
+            this.txtBookAuthorName.Name = "txtBookAuthorName";
+            this.txtBookAuthorName.Size = new System.Drawing.Size(200, 20);
+            this.txtBookAuthorName.TabIndex = 7;
+            // 
+            // txtBookPublication
+            // 
+            this.txtBookPublication.Location = new System.Drawing.Point(207, 157);
+            this.txtBookPublication.Name = "txtBookPublication";
+            this.txtBookPublication.Size = new System.Drawing.Size(200, 20);
+            this.txtBookPublication.TabIndex = 8;
+            // 
+            // txtBookPrice
+            // 
+            this.txtBookPrice.Location = new System.Drawing.Point(207, 222);
+            this.txtBookPrice.Name = "txtBookPrice";
+            this.txtBookPrice.Size = new System.Drawing.Size(200, 20);
+            this.txtBookPrice.TabIndex = 10;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(207, 260);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(200, 20);
+            this.txtQuantity.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(86, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Book ID";
+            // 
+            // txtBookID
+            // 
+            this.txtBookID.Location = new System.Drawing.Point(207, 54);
+            this.txtBookID.Name = "txtBookID";
+            this.txtBookID.ReadOnly = true;
+            this.txtBookID.Size = new System.Drawing.Size(200, 20);
+            this.txtBookID.TabIndex = 14;
+            // 
+            // dtpBookPurchaseDate
+            // 
+            this.dtpBookPurchaseDate.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dtpBookPurchaseDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpBookPurchaseDate.Location = new System.Drawing.Point(207, 187);
+            this.dtpBookPurchaseDate.Name = "dtpBookPurchaseDate";
+            this.dtpBookPurchaseDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpBookPurchaseDate.TabIndex = 16;
+            // 
+            // add_books
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 494);
+            this.Controls.Add(this.dtpBookPurchaseDate);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.txtBookID);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtBookPrice);
+            this.Controls.Add(this.txtBookPublication);
+            this.Controls.Add(this.txtBookAuthorName);
+            this.Controls.Add(this.txtBookName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "add_books";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add New Books";
+            this.Load += new System.EventHandler(this.add_books_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.TextBox txtBookAuthorName;
+        private System.Windows.Forms.TextBox txtBookPublication;
+        private System.Windows.Forms.TextBox txtBookPrice;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtBookID;
+        private System.Windows.Forms.DateTimePicker dtpBookPurchaseDate;
+    }
+}
---
+++ LibraryManagementSystem/LibraryManagementSystem/add_books.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagementSystem
+{
+    public partial class add_books : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-E30J54Q\SQLEXPRESS;Initial Catalog=LMS;Integrated Security=True;Pooling=False");
+        int BookID;
+        public add_books()
+        {
+            InitializeComponent();
+        }
+
+        public void refesh()
+        {
+            txtBookName.Text = "";
+            txtBookAuthorName.Text = "";
+            txtBookPublication.Text = "";
+            dtpBookPurchaseDate.Value = DateTime.Now;
+            txtBookPrice.Text = "";
+            txtQuantity.Text = "";
+        }
+
+        private bool checkData()
+        {
+            if (string.IsNullOrWhiteSpace(txtBookName.Text))
+            {
+                MessageBox.Show("Book Name not empty", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtBookName.Focus();
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtBookAuthorName.Text))
+            {
+                MessageBox.Show("Book Author Name not empty", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtBookAuthorName.Focus();
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtBookPublication.Text))
+            {
+                MessageBox.Show("Publication not empty", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtBookPublication.Focus();
+                return false;
+            }
+           
+            if (string.IsNullOrWhiteSpace(txtBookPrice.Text))
+            {
+                MessageBox.Show("Book Price not empty", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtBookPrice.Focus();
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtQuantity.Text))
+            {
+                MessageBox.Show("Quantity not empty", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtQuantity.Focus();
+                return false;
+            }
+
+
+            return true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+                if (checkData())
+                {
+                    con.Open();
+                    SqlCommand cmd = con.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "insert into books_info values('" + txtBookName.Text + "','" + txtBookAuthorName.Text + "','" + txtBookPublication.Text + "','" + dtpBookPurchaseDate.Text + "'," + txtBookPrice.Text + "," + txtQuantity.Text + ")";
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    
+                    MessageBox.Show("Add book successfully", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    btnNew.Enabled = true;
+                    btnSave.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("Vui lòng nhập lại :) ", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Select MAX(Id) from books_info";
+                cmd.ExecuteNonQuery();
+                BookID = Convert.ToInt32(cmd.ExecuteScalar());
+                con.Close();
+            }
+            catch
+            {
+                BookID = 0;
+            }
+
+            BookID++;
+           
+            refesh();
+            txtBookID.Text = BookID.ToString();
+            btnNew.Enabled = false;
+            btnSave.Enabled = true;
+            txtBookName.Focus();
+        }
+
+        private void add_books_Load(object sender, EventArgs e)
+        {
+            btnSave.Enabled = false;
+        }
+    }
+}
