47f27a01d749260235fc8a0781bd15afb9ad3725
nvan23 <dtnvan230898@gmail.com>
2020-05-05 18:47:31 +0700

    Create view student function and design view student form

---
+++ LibraryManagementSystem/LibraryManagementSystem/view_student_info.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace LibraryManagementSystem
+{
+    partial class view_student_info
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvStudent = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvStudent
+            // 
+            this.dgvStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudent.Location = new System.Drawing.Point(261, 12);
+            this.dgvStudent.Name = "dgvStudent";
+            this.dgvStudent.Size = new System.Drawing.Size(967, 337);
+            this.dgvStudent.TabIndex = 0;
+            this.dgvStudent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvStudent_CellContentClick);
+            // 
+            // view_student_info
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1240, 572);
+            this.Controls.Add(this.dgvStudent);
+            this.Name = "view_student_info";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "View Student Infomation";
+            this.Load += new System.EventHandler(this.view_student_info_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudent)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvStudent;
+    }
+}
---
+++ LibraryManagementSystem/LibraryManagementSystem/view_student_info.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagementSystem
+{
+    public partial class view_student_info : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-E30J54Q\SQLEXPRESS;Initial Catalog=LMS;Integrated Security=True;Pooling=False");
+
+        public view_student_info()
+        {
+            InitializeComponent();
+        }
+
+        private void view_student_info_Load(object sender, EventArgs e)
+        {
+            int index = 0;
+
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            try
+            {
+                
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from student_info";
+                cmd.ExecuteNonQuery();
+
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                dgvStudent.DataSource = dt;
+
+                Bitmap img;
+                DataGridViewImageColumn imageCol = new DataGridViewImageColumn();
+                imageCol.Width = 500;
+                imageCol.HeaderText = "Student Image";
+                imageCol.ImageLayout = DataGridViewImageCellLayout.Zoom;
+                imageCol.Width = 100;
+                dgvStudent.Columns.Add(imageCol);
+
+                foreach(DataRow dr in dt.Rows)
+                {
+                    img = new Bitmap(@"..\..\" + dr["student_image"].ToString());
+                    dgvStudent.Rows[index].Cells[8].Value = img;
+                    dgvStudent.Rows[index].Height = 100;
+                    index = index + 1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        }
+
+        private void dgvStudent_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
